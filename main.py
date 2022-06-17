600460a41b84e08e40dc36ba7189b21d135c5f4c
Lazynx <frichaplay@gmail.com>
2022-06-17 16:06:06 +0600

    Создание бота, который реагирует на /hello-world

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+
+if __name__ == '__main__':
+    bot = telebot.TeleBot('5360344539:AAHmp4MnK8DQFMHgHE0TBWySTRiA9mHNcZQ')
+
+
+    @bot.message_handler(content_types=['text'])
+    def get_text_messages(message):
+        if message.text == '/hello-world':
+            bot.send_message(message.from_user.id, 'Привет мир!')
+        elif message.text == 'Привет':
+            mess = 'Привет {} {}'.format(message.from_user.first_name, message.from_user.last_name)
+            bot.send_message(message.from_user.id, mess)
+        if message.text == '/help':
+            bot.send_message(message.from_user.id, 'У меня есть команда /hello-world')
+        else:
+            bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
+
+
+    bot.polling(none_stop=True, interval=0)
